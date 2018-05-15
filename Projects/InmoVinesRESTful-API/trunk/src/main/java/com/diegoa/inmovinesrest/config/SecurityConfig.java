331a5c1acc446b0df7820210c324fa42897d839c
Daniel Arroyo <daniarromay@gmail.com>
2018-05-15 20:19:05 +0200

    Security rest casi a full

--- Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/config/SecurityConfig.java
+++ Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/config/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.diegoa.inmovinesrest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // Authentication : User --> Roles
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance()).withUser("user1").password("secret1")
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("user1").password("secret1")
                 .roles("USER").and().withUser("admin1").password("secret1")
                 .roles("USER", "ADMIN");
     }
 
     // Authorization : Role -> Access
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers("/students/**")
+        http.httpBasic().and().authorizeRequests().antMatchers("/**")
                 .hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
                 .csrf().disable().headers().frameOptions().disable();
     }
--- Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/controllers/InmueblesController.java
+++ Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/controllers/InmueblesController.java
@@ -4,8 +4,9 @@ package com.diegoa.inmovinesrest.controllers;
 import com.diegoa.inmovinesrest.entities.inmuebles.Inmuebles;
 import com.diegoa.inmovinesrest.services.inmuebles.impl.InmueblesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,11 +18,11 @@ public class InmueblesController {
     InmueblesServiceImpl inmueblesService;
 
 
-    @RequestMapping("/inmueble")
-    @PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
-    public Inmuebles getInmueble() {
+    @GetMapping("/hola")
+    @ResponseBody
+    public String getInmueble() {
 
-        return inmueblesService.listAll().get(0);
+        return "hi";
     }
 
 
