1d20d178e59827ccc08a66cd738b772f38ef117f
Daniel Arroyo Mayo <37345135+karat0s@users.noreply.github.com>
2018-05-29 01:27:41 +0200

    Inmuebles Serializer
    Inmuebles Serializer terminado

--- Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/entities/clientes/ClientesSerializer.java
+++ Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/entities/clientes/ClientesSerializer.java
@@ -1,6 +1,5 @@
 package com.diegoa.inmovinesrest.entities.clientes;
 
-import com.diegoa.inmovinesrest.entities.inmuebles.Inmuebles;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
@@ -14,19 +13,19 @@ public class ClientesSerializer extends JsonSerializer {
         Clientes c = (Clientes) o;
 
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeNumberField("id",c.getId());
+        jsonGenerator.writeNumberField("id", c.getId());
         jsonGenerator.writeStringField("name", c.getDatosPersonales().getNombre());
         jsonGenerator.writeStringField("surname", c.getDatosPersonales().getApellidos());
         jsonGenerator.writeStringField("DNI", c.getDatosPersonales().getDni());
         jsonGenerator.writeStringField("mail", c.getDatosPersonales().getMail());
         jsonGenerator.writeStringField("tlf", c.getDatosPersonales().getTelefono());
-            jsonGenerator.writeFieldName("agenteEntrada");
-                jsonGenerator.writeStartObject();
-                jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getNombre());
-                jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getApellidos());
-                jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getMail());
-                jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getFacebook());
-                jsonGenerator.writeEndObject();
+        jsonGenerator.writeFieldName("agenteEntrada");
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getNombre());
+        jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getApellidos());
+        jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getMail());
+        jsonGenerator.writeStringField("nombre", c.getAgenteEntrada().getFacebook());
+        jsonGenerator.writeEndObject();
         jsonGenerator.writeEndObject();
 
 
--- Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/entities/inmuebles/PageInmueblesSerializer.java
+++ Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/entities/inmuebles/PageInmueblesSerializer.java
@@ -19,13 +19,55 @@ public class PageInmueblesSerializer extends JsonSerializer {
 
         for (Inmuebles i : p.getContent()) {
 
+            //Caracteristicas basicas
             jsonGenerator.writeStartObject();
             jsonGenerator.writeNumberField("id", i.getId());
-
-
+            jsonGenerator.writeStringField("descripcion", i.getDescripcion());
+            jsonGenerator.writeStringField("textoReclamo", i.getTextoReclamo());
+            jsonGenerator.writeNumberField("gastosComunidad", i.getGastosComunidad());
             jsonGenerator.writeStringField("tipo_vivienda", i.getTipos().getNombre());
             jsonGenerator.writeStringField("tipo_gestion", i.getGestiones().getNombre());
+            //Caracteristicas principales del inmueble
+            jsonGenerator.writeObjectFieldStart("caracteristicas");
+            jsonGenerator.writeNumberField("nHabitaciones", i.getCaracteristicas().getnHabitaciones());
+            jsonGenerator.writeNumberField("nBanos", i.getCaracteristicas().getnBanos());
+            jsonGenerator.writeNumberField("m2Utiles", i.getCaracteristicas().getM2Utiles());
+            jsonGenerator.writeStringField("garaje", String.valueOf(i.getCaracteristicas().getGaraje()));
+            jsonGenerator.writeStringField("ascensor", String.valueOf(i.getCaracteristicas().getAscensor()));
             jsonGenerator.writeEndObject();
+            //Localizacion del inmueble
+            jsonGenerator.writeObjectFieldStart("localizacion");
+            //Comunidad
+            jsonGenerator.writeObjectFieldStart("comunidad");
+            jsonGenerator.writeNumberField("id", i.getLocalizacion().getComunidad().getId());
+            jsonGenerator.writeStringField("comunidad", i.getLocalizacion().getComunidad().getComunidad());
+            jsonGenerator.writeStringField("slug", i.getLocalizacion().getComunidad().getSlug());
+            jsonGenerator.writeEndObject();
+            //Provincia
+            jsonGenerator.writeObjectFieldStart("provincia");
+            jsonGenerator.writeNumberField("id", i.getLocalizacion().getProvincia().getId());
+            jsonGenerator.writeStringField("provincia", i.getLocalizacion().getProvincia().getProvincia());
+            jsonGenerator.writeStringField("slug", i.getLocalizacion().getProvincia().getSlug());
+            jsonGenerator.writeEndObject();
+            //Zona
+            jsonGenerator.writeObjectFieldStart("zona");
+            jsonGenerator.writeNumberField("id", i.getLocalizacion().getZona().getId());
+            jsonGenerator.writeStringField("nombre", i.getLocalizacion().getZona().getNombre());
+            jsonGenerator.writeStringField("activa", String.valueOf(i.getLocalizacion().getZona().getActiva()));
+            jsonGenerator.writeStringField("nombreAdmin", i.getLocalizacion().getZona().getNombreAdmin());
+            jsonGenerator.writeEndObject();
+            //Poblacion
+            jsonGenerator.writeObjectFieldStart("poblacion");
+            jsonGenerator.writeNumberField("id", i.getLocalizacion().getPoblacion().getId());
+            jsonGenerator.writeStringField("municipio", i.getLocalizacion().getPoblacion().getMunicipio());
+            jsonGenerator.writeStringField("slug", i.getLocalizacion().getPoblacion().getSlug());
+            jsonGenerator.writeEndObject();
+
+            jsonGenerator.writeEndObject();
+
+            jsonGenerator.writeEndObject();
+
+
         }
 
         jsonGenerator.writeEndArray();
@@ -36,16 +78,16 @@ public class PageInmueblesSerializer extends JsonSerializer {
         jsonGenerator.writeStringField("sorted", String.valueOf(p.getPageable().getSort().isSorted()));
         jsonGenerator.writeEndObject();
         // TODO esto esta bien??
-        jsonGenerator.writeNumberField("offset",p.getPageable().getOffset());
+        jsonGenerator.writeNumberField("offset", p.getPageable().getOffset());
         jsonGenerator.writeNumberField("pageNumber", p.getNumber());
         jsonGenerator.writeNumberField("pageSize", p.getTotalPages());
         jsonGenerator.writeStringField("paged", String.valueOf(p.getPageable().isPaged()));
         jsonGenerator.writeStringField("unpaged", String.valueOf(p.getPageable().isUnpaged()));
         jsonGenerator.writeEndObject();
 
-        jsonGenerator.writeStringField("last",String.valueOf(p.isLast()));
+        jsonGenerator.writeStringField("last", String.valueOf(p.isLast()));
         jsonGenerator.writeNumberField("totalElements", p.getTotalElements());
-        jsonGenerator.writeNumberField("totalPages",p.getTotalPages());
+        jsonGenerator.writeNumberField("totalPages", p.getTotalPages());
         jsonGenerator.writeNumberField("size", p.getSize());
         jsonGenerator.writeNumberField("number", p.getNumber());
         jsonGenerator.writeObjectFieldStart("sort");
@@ -53,7 +95,7 @@ public class PageInmueblesSerializer extends JsonSerializer {
         jsonGenerator.writeStringField("sorted", String.valueOf(p.getPageable().getSort().isSorted()));
         jsonGenerator.writeEndObject();
 
-        jsonGenerator.writeNumberField("numberOfElements",p.getNumberOfElements());
+        jsonGenerator.writeNumberField("numberOfElements", p.getNumberOfElements());
         jsonGenerator.writeStringField("first", String.valueOf(p.isFirst()));
         jsonGenerator.writeEndObject();
 
