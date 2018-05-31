867ae2233da8339759ab30d453e0cc8b1bb029c0
Daniel Arroyo Mayo <37345135+karat0s@users.noreply.github.com>
2018-06-01 00:32:04 +0200

    Controlador AdminAgentesController

--- Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/controllers/secured/agentes/AdminAgentesController.java
+++ Projects/InmoVinesRESTful-API/trunk/src/main/java/com/diegoa/inmovinesrest/controllers/secured/agentes/AdminAgentesController.java
@@ -1,11 +1,44 @@
 package com.diegoa.inmovinesrest.controllers.secured.agentes;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.diegoa.inmovinesrest.entities.agentes.Agentes;
+import com.diegoa.inmovinesrest.services.agentes.impl.AgentesServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:8087","http://localhost:8080" })
+/**
+ * @author Daniel Arroyo
+ * @since 0.0.1
+ */
+
+@CrossOrigin(origins = {"http://localhost:8087", "http://localhost:8080"})
 @RestController
-@RequestMapping("user")
+@RequestMapping("admin")
 public class AdminAgentesController {
+
+    @Autowired
+    AgentesServiceImpl agentesService;
+
+    @RequestMapping(value = "/agentes", produces = "application/json", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Page<Agentes>> getAgentes(Pageable pageable) throws JsonProcessingException {
+
+        Page<Agentes> page = agentesService.listAllByPage(pageable);
+
+        return new ResponseEntity(page, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/agentes/{id}", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<Agentes> getAgenteById(@PathVariable("id") long id, Pageable pageable) throws JsonProcessingException {
+
+        Agentes agentes = agentesService.findOneById(id);
+
+        return new ResponseEntity(agentes, HttpStatus.OK);
+    }
 }
